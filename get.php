807cc3d40838bc862e8943647a84bb75b326f0d0
Victor Lelis <39639652+victorlelis@users.noreply.github.com>
2020-04-04 17:38:13 -0300

    Verificação em duas fontes

--- get.php
+++ get.php
@@ -1,4 +1,5 @@
 <?php
+// API
 
 $brazil = file_get_contents('https://covid19-brazil-api.now.sh/api/report/v1/brazil');
 
@@ -111,73 +112,32 @@ for ($i = 0; $i < $len; $i++) {
   ]];
 }
 
-
 $new = [
   "brazil" => [
     "updated" => time(),
     "states" => $statesUp,
-    "confirmed" => $brazil['cases'],
+    "confirmed" => $brazil['confirmed'],
     "recovered" => $brazil['recovered'],
     "deaths" => $brazil['deaths']
-  ],
-  "old" => []
+  ]
 ];
 
 $time = time();
 
-if (!is_dir('old')) {
-  mkdir('old', 0777, true);
+if (!is_dir('backup')) {
+  mkdir('backup', 0777, true);
 }
 
 if (!file_exists('data.json')) {
   $json = fopen('data.json', 'w');
-  $new['old'] = $time;
   $new = json_encode($new);
   fwrite($json, $new);
   fclose($json);
   echo "data.json criado com sucesso! <br>";
 
-  $txt = fopen('data.txt', 'a');
-  fwrite($txt, "data.json" . PHP_EOL);
-  fclose($txt);
-  echo "data.txt criado com sucesso! <br>";
-
-  copy('data.json', 'old/data.json');
+  copy('data.json', 'backup/data.json');
   echo "backup realizado com sucesso! <br>";
 } else {
-
-  function old($time)
-  {
-    copy('data.json', 'old/data.json');
-    echo "backup realizado com sucesso! <br>";
-
-    $old = file_get_contents('data.json');
-    $old = json_decode($old, true);
-
-    if (is_int($old['old'])) {
-      $old['old'] = [
-        $old['old'],
-        $time
-      ];
-    } else {
-      array_push($old['old'], $time);
-    }
-    $old = json_encode($old);
-
-    $jsonOld = fopen('data.json', 'w');
-    fwrite($jsonOld, $old);
-    fclose($jsonOld);
-
-    $oldJson = fopen('old/' . $time . '.json', 'w');
-    fwrite($oldJson, $old);
-    fclose($oldJson);
-    echo "backup old/{$time}.json criado com sucesso! <br>";
-
-    $txt = fopen('data.txt', 'a');
-    fwrite($txt, $time . PHP_EOL);
-    fclose($txt);
-    echo "LOG: {$time} foi escrito em data.txt <br>";
-  }
 
   $check = file_get_contents('data.json');
   $check = json_decode($check, true);
@@ -191,7 +151,7 @@ if (!file_exists('data.json')) {
         $data = json_decode($data, true);
         $data['brazil']['updated'] = $time;
         $data['brazil']['states'] = $statesUp;
-        $data['brazil']['confirmed'] = $brazil['cases'];
+        $data['brazil']['confirmed'] = $brazil['confirmed'];
         $data['brazil']['recovered'] = $brazil['recovered'];
         $data['brazil']['deaths'] = $brazil['deaths'];
 
@@ -200,8 +160,6 @@ if (!file_exists('data.json')) {
         fwrite($json, $data);
         fclose($json);
         echo "informações nos estados atualizada! <br>";
-
-        old($time);
 
         return false;
       }
@@ -216,7 +174,7 @@ if (!file_exists('data.json')) {
       $data = json_decode($data, true);
       $data['brazil']['updated'] = $time;
       $data['brazil']['states'] = $statesUp;
-      $data['brazil']['confirmed'] = $brazil['cases'];
+      $data['brazil']['confirmed'] = $brazil['confirmed'];
       $data['brazil']['recovered'] = $brazil['recovered'];
       $data['brazil']['deaths'] = $brazil['deaths'];
 
@@ -229,12 +187,12 @@ if (!file_exists('data.json')) {
   }
 
 
-  if ($check['brazil']['confirmed'] != $brazil['cases'] || $check['brazil']['recovered'] != $brazil['recovered'] || $check['brazil']['deaths'] != $brazil['deaths']) {
+  if ($check['brazil']['confirmed'] != $brazil['confirmed'] || $check['brazil']['recovered'] != $brazil['recovered'] || $check['brazil']['deaths'] != $brazil['deaths']) {
     $data = file_get_contents('data.json');
     $data = json_decode($data, true);
     $data['brazil']['updated'] = $time;
     $data['brazil']['states'] = $statesUp;
-    $data['brazil']['confirmed'] = $brazil['cases'];
+    $data['brazil']['confirmed'] = $brazil['confirmed'];
     $data['brazil']['recovered'] = $brazil['recovered'];
     $data['brazil']['deaths'] = $brazil['deaths'];
 
@@ -244,9 +202,97 @@ if (!file_exists('data.json')) {
     fclose($json);
     echo "informações no Brasil atualizada! <br>";
   }
+}
 
-  if ($check['brazil']['confirmed'] != $brazil['cases'] || $check['brazil']['deaths'] != $brazil['deaths']) {
-    old($time);
+// Brasil.io
+// Compara informações mais atualizada e atualiza
+$get = [
+  'AC' => 'Acre',
+  'AL' => 'Alagoas',
+  'AP' => 'Amapá',
+  'AM' => 'Amazonas',
+  'BA' => 'Bahia',
+  'CE' => 'Ceará',
+  'DF' => 'Distrito Federal',
+  'ES' => 'Espírito Santo',
+  'GO' => 'Goiás',
+  'MA' => 'Maranhão',
+  'MT' => 'Mato Grosso',
+  'MS' => 'Mato Grosso do Sul',
+  'MG' => 'Minas Gerais',
+  'PA' => 'Pará',
+  'PB' => 'Paraíba',
+  'PR' => 'Paraná',
+  'PE' => 'Pernambuco',
+  'PI' => 'Piauí',
+  'RJ' => 'Rio de Janeiro',
+  'RN' => 'Rio Grande do Norte',
+  'RS' => 'Rio Grande do Sul',
+  'RO' => 'Rondônia',
+  'RR' => 'Roraima',
+  'SC' => 'Santa Catarina',
+  'SP' => 'São Paulo',
+  'SE' => 'Sergipe',
+  'TO' => 'Tocantins'
+];
+
+$states = [];
+
+foreach ($get as $key => $state) {
+  $item = file_get_contents("https://brasil.io/api/dataset/covid19/caso/data?state={$key}&is_last=True&place_type=state&format=json");
+
+  $item = json_decode($item, true);
+  $item = $item['results'][0];
+
+  $states += [strtolower($item['state']) => [
+    'confirmed' => $item['confirmed'],
+    'deaths' => $item['deaths'],
+  ]];
+}
+
+$brasilio = $states;
+$data = file_get_contents("data.json");
+$data = json_decode($data, true);
+$data = $data['brazil']['states'];
+
+
+$modified = false;
+
+foreach ($data as $key => $value) {
+  if ($data[$key]['confirmed'] != $brasilio[$key]['confirmed']) {
+    if ($brasilio[$key]['confirmed'] > $data[$key]['confirmed']) {
+      $data[$key]['confirmed'] = $brasilio[$key]['confirmed'];
+      echo 'confirmed <br>';
+      $modified = true;
+    }
+    if ($brasilio[$key]['deaths'] > $data[$key]['deaths']) {
+      $data[$key]['deaths'] = $brasilio[$key]['deaths'];
+      echo 'deaths <br>';
+      $modified = true;
+    }
   }
 }
-echo "sem novas informações!";
+
+if ($modified) {
+  $novo = file_get_contents("data.json");
+  $novo = json_decode($novo, true);
+  $states = $novo['brazil']['states'] = $data;
+  $novo = [
+    "brazil" => [
+      "updated" => time(),
+      "states" => $states,
+      "confirmed" => $novo['brazil']['confirmed'],
+      "recovered" => $novo['brazil']['recovered'],
+      "deaths" => $novo['brazil']['deaths']
+    ]
+  ];
+
+  $json = fopen('data.json', 'w');
+  $novo = json_encode($novo);
+  fwrite($json, $novo);
+  fclose($json);
+
+  echo 'Divergência entre API e Brasi.io - Atualizado!';
+}
+
+echo "sem novas informações! <br>";
